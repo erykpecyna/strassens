cabe6db8f34f0d19df08e163a39402f74f307a6c
Eryk <eryk.pecyna@gmail.com>
2020-03-26 22:31:33 +0000

    strassens with cutoff

--- strassen.cpp
+++ strassen.cpp
@@ -2,7 +2,10 @@
 #include <fstream>
 #include <type_traits>
 
-const int MAXLINE = 256;
+
+#define MAXLINE 256
+#define CUTOFF 600
+
 
 // NOTE: All matrix arithmetic assumes equal dimensions
 // (No error handling)
@@ -85,7 +88,7 @@ struct Matrix {
 
 	// For testing
 	void printMatrix() {
-		for(int row = 0; row < d; row++) {
+		for(int row = 0; row < actuald; row++) {
 			printf("%i ", get(row,row));
 			printf("\n");
 		}
@@ -244,79 +247,81 @@ Matrix* multiplyMatricesStandard(M* m1, T* m2) {
 	return res;
 }
 
-template <class M>
-Matrix multiplyMatricesStrassens(M* m1, M* m2, int parentd) {
-	subMat* s1 = splitMatrix(m1);
-	subMat* s2 = splitMatrix(m2);
-	subMat* A = &s1[0];
-	subMat* B = &s1[1];
-	subMat* C = &s1[2];
-	subMat* D = &s1[3];
-	subMat* E = &s2[0];
-	subMat* F = &s2[1];
-	subMat* G = &s2[2];
-	subMat* H = &s2[3];
+template <class M, class T>
+Matrix* multiplyMatricesStrassens(M* m1, T* m2, int parentd) {
+	if(parentd > CUTOFF) {
+		subMat* s1 = splitMatrix(m1);
+		subMat* s2 = splitMatrix(m2);
+		subMat* A = &s1[0];
+		subMat* B = &s1[1];
+		subMat* C = &s1[2];
+		subMat* D = &s1[3];
+		subMat* E = &s2[0];
+		subMat* F = &s2[1];
+		subMat* G = &s2[2];
+		subMat* H = &s2[3];
 
-	Matrix* FmH = addMatrices(F, H, false);
-	Matrix* AB = addMatrices(A, B);
-	Matrix* CD = addMatrices(C, D);
-	Matrix* GmE = addMatrices(G, E, false);
-	Matrix* AD = addMatrices(A, D);
-	Matrix* EH = addMatrices(E, H);
-	Matrix* BmD = addMatrices(B, D, false);
-	Matrix* GH = addMatrices(G, H);
-	Matrix* AmC = addMatrices(A, C, false);
-	Matrix* EF = addMatrices(E, F);
+		Matrix* FmH = addMatrices(F, H, false);
+		Matrix* AB = addMatrices(A, B);
+		Matrix* CD = addMatrices(C, D);
+		Matrix* GmE = addMatrices(G, E, false);
+		Matrix* AD = addMatrices(A, D);
+		Matrix* EH = addMatrices(E, H);
+		Matrix* BmD = addMatrices(B, D, false);
+		Matrix* GH = addMatrices(G, H);
+		Matrix* AmC = addMatrices(A, C, false);
+		Matrix* EF = addMatrices(E, F);
 
-	Matrix* p1 = multiplyMatricesStandard(A, FmH);
-	Matrix* p2 = multiplyMatricesStandard(AB, H);
-	Matrix* p3 = multiplyMatricesStandard(CD, E);
-	Matrix* p4 = multiplyMatricesStandard(D, GmE);
-	Matrix* p5 = multiplyMatricesStandard(AD, EH);
-	Matrix* p6 = multiplyMatricesStandard(BmD, GH);
-	Matrix* p7 = multiplyMatricesStandard(AmC, EF);
+		Matrix* p1 = multiplyMatricesStrassens(A, FmH, A->d);
+		Matrix* p2 = multiplyMatricesStrassens(AB, H, H->d);
+		Matrix* p3 = multiplyMatricesStrassens(CD, E, E->d);
+		Matrix* p4 = multiplyMatricesStrassens(D, GmE, D->d);
+		Matrix* p5 = multiplyMatricesStrassens(AD, EH, AD->d);
+		Matrix* p6 = multiplyMatricesStrassens(BmD, GH, GH->d);
+		Matrix* p7 = multiplyMatricesStrassens(AmC, EF, EF->d);
 
-	Matrix* p5p4 = addMatrices(p5, p4);
-	Matrix* p2mp6 = addMatrices(p2,p6,false);
-	Matrix* p5p1 = addMatrices(p5, p1);
-	Matrix* p3p7 = addMatrices(p3, p7);
+		Matrix* p5p4 = addMatrices(p5, p4);
+		Matrix* p2mp6 = addMatrices(p2,p6,false);
+		Matrix* p5p1 = addMatrices(p5, p1);
+		Matrix* p3p7 = addMatrices(p3, p7);
 
-	Matrix* q1 = addMatrices(p5p4, p2mp6, false);
-	Matrix* q2 = addMatrices(p1, p2);
-	Matrix* q3 = addMatrices(p3, p4);
-	Matrix* q4 = addMatrices(p5p1, p3p7, false);
+		Matrix* q1 = addMatrices(p5p4, p2mp6, false);
+		Matrix* q2 = addMatrices(p1, p2);
+		Matrix* q3 = addMatrices(p3, p4);
+		Matrix* q4 = addMatrices(p5p1, p3p7, false);
 
-	Matrix res(q1, q2, q3, q4, parentd);
+		Matrix* res = new Matrix(q1, q2, q3, q4, parentd);
 
 
-	delete s1;
-	delete s2;
-	delete FmH;
-	delete AB;
-	delete CD;
-	delete GmE;
-	delete AD;
-	delete EH;
-	delete BmD;
-	delete GH;
-	delete AmC;
-	delete EF;
-	delete p5p4;
-	delete p2mp6;
-	delete p5p1;
-	delete p3p7;
-	delete p1;
-	delete p2;
-	delete p3;
-	delete p4;
-	delete p5;
-	delete p6;
-	delete p7;
-	delete q1;
-	delete q2;
-	delete q3;
-	delete q4;
-	return res;
+		delete s1;
+		delete s2;
+		delete FmH;
+		delete AB;
+		delete CD;
+		delete GmE;
+		delete AD;
+		delete EH;
+		delete BmD;
+		delete GH;
+		delete AmC;
+		delete EF;
+		delete p5p4;
+		delete p2mp6;
+		delete p5p1;
+		delete p3p7;
+		delete p1;
+		delete p2;
+		delete p3;
+		delete p4;
+		delete p5;
+		delete p6;
+		delete p7;
+		delete q1;
+		delete q2;
+		delete q3;
+		delete q4;
+		return res;
+	} else return multiplyMatricesStandard(m1, m2);
 }
 
 int main(int argc, char* argv[]) {
@@ -337,40 +342,10 @@ int main(int argc, char* argv[]) {
 		m2.mat[i] = std::stoi(buff);
 	}
 
-	// m1.printMatrix();
-	// printf("\n");
-	// m2.printMatrix();
+	Matrix* mults = multiplyMatricesStrassens(&m1, &m2, d);
 
+	mults->actuald = d;
+	mults->printMatrix();
 
-	// Matrix* mult = multiplyMatricesStandard(&m1, &m2);
-	Matrix mults = multiplyMatricesStrassens(&m1, &m2, d);
-	
-	// printf("\n");
-
-	// mult->printMatrix();
-	// printf("\n");
-	mults.printMatrix();
-
-	
-	// subMat* arr = splitMatrix(&m1);
-
-	// for(int i = 0; i < 4; i++) {
-	// 	printf("\n"); 
-	// 	arr[i].printMatrix();
-	// 	printf("\n"); 
-	// }
-
-
-
-	// subMat* sarr = splitMatrix(&arr[0]);
-
-	// for(int i = 0; i < 4; i++) {
-	// 	printf("\n"); 
-	// 	sarr[i].printMatrix();
-	// 	printf("\n"); 
-	// }
-
-	// Matrix testmult = multiplyMatricesStandard(&arr[0], &arr[1]);
-	// testmult.printMatrix();
-
+	delete mults;
 }
